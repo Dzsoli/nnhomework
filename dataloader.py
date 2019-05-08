c034763f44cba433ac51c6caf9051992fe3f9ec0
rakoso <rakosoliver827@gmail.com>
2019-05-08 10:16:00 +0200

    Unstable. Started to work on classes for Dataset and Vehicles

--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,70 @@
 import csv
 import torch
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
+
+
+class VehicleDataset(Dataset):
+    """NGSIM vehicle dataset"""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        # TODO: megmagyarázni a változó neveket és argumentumokat (angolul
+        """
+        asd
+        """
+        self.all_data = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        """returns with all the number of frames of the dataset"""
+        return len(self.all_data)
+
+    def __getitem__(self, idx):
+        """returns the idx_th vehicle"""
+        return VehicleData()
+
+
+class VehicleData:
+    """class for Vehicles. Instantiate in VehicleDataset.__getitem__"""
+    def __init__(self, data):
+        # car ID
+        self.id = int(data[0, 0])
+        # frame ID
+        self.frames = data[:, 1]
+        # total frame number
+        self.size = int(data[0, 2])
+        # global time
+        self.t = data[:, 3]
+        # lateral x coordinate
+        self.x = data[:, 4]
+        # Longitudinal y coordinate
+        self.y = data[:, 5]
+        # Dimensions of the car: Length, Width
+        self.dims = data[0, 8:10]
+        # Type, 1-motor, 2-car, 3-truck
+        self.type = int(data[0, 10])
+        # Instantenous velocity
+        self.v = data[:, 11]
+        # Instantenous acceleration
+        self.a = data[:, 12]
+        # lane ID: 1 is the FARTHEST LEFT. 5 is the FARTHEST RIGHT.
+        # 6 is Auxiliary lane for off and on ramp
+        # 7 is on ramp
+        # 8 is off ramp
+        self.lane_id = data[:, 13]
+        # [None] if no lane change; [+/-1, frame] if there is a lane change in the specific frame
+        # [0, frame_id] or [-1, frame_id] or [1, frame_id]
+        self.change_lane = None
+        # mean, variance, changes or not?, frame id
 
 
 def data_sampler(window_size, _csv_name, data_save):
     if data_save:
         with open(_csv_name) as f:
-            read_csv = csv.reader(f, delimiter=';')
+            read_csv = csv.reader(f, delimiter=',')
             header = next(read_csv)
             data = []
 
